87b67ee116afc8b3b627739407f2739a98ea8731
pratham9634 <prathampetwal100@gmail.com>
2025-05-27 22:09:54 +0530

    add cron job

--- src/app/api/delete-expired/route.js
+++ src/app/api/delete-expired/route.js
@@ -1,10 +1,13 @@
-
+import { NextResponse } from 'next/server';
 
 import File from '@/models/File'
 import cloudinary from '@/lib/cloudinary'
 import dbConnect from '@/lib/mongodb'
 
 export const GET = async () => {
+  if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+    return res.status(401).end('Unauthorized');
+  }
   await dbConnect()
   const expired = await File.find({ expireAt: { $lte: new Date() } })
 
@@ -12,6 +15,5 @@ export const GET = async () => {
     await cloudinary.uploader.destroy(file.cloudinaryId, { resource_type: 'auto' })
     await file.deleteOne()
   }
-
-  return new Response('Expired files cleaned')
+  return NextResponse.json({message:"cron job ran at : "+ new Date()});
 }
